dc1fbf21684771cbd7847a254a63705876f5b460
jeborivo <borarooney@gmail.com>
2017-07-05 12:21:51 +0200

    themep2

--- sites/all/themes/gitslike/templates/nodes/node--zadovoljni-kupci--teaser.tpl.php
+++ sites/all/themes/gitslike/templates/nodes/node--zadovoljni-kupci--teaser.tpl.php
@@ -84,34 +84,16 @@
   <header>
     <?php print render($title_prefix); ?>
     <?php if (!$page && !empty($title)): ?>
-    <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
+    <h4<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h4>
     <?php endif; ?>
     <?php print render($title_suffix); ?>
     <?php if ($display_submitted): ?>
-    <span class="submitted">
-      <?php print $user_picture; ?>
-      <?php print $submitted; ?>
-    </span>
     <?php endif; ?>
   </header>
   <?php endif; ?>
   <?php
     // Hide comments, tags, and links now so that we can render them later.
-    hide($content['comments']);
-    hide($content['links']);
-    hide($content['field_tags']);
-    print render($content);
+    print render($content['field_kupci_slika']);
   ?>
-  <?php
-    // Only display the wrapper div if there are tags or links.
-    $field_tags = render($content['field_tags']);
-    $links = render($content['links']);
-    if ($field_tags || $links):
-  ?>
-   <footer>
-     <?php print $field_tags; ?>
-     <?php print $links; ?>
-  </footer>
-  <?php endif; ?>
-  <?php print render($content['comments']); ?>
+
 </article>
